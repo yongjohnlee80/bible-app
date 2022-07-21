16f8ceb13e85ebb09fb59e7e572540d6117b81d3
yongjohnlee80 <yongjohnlee80@outlook.com>
2022-07-21 00:10:14 -0700

    added custom httpRequestHook

--- src/app/models/bible.books.model.ts
+++ src/app/models/bible.books.model.ts
@@ -84,4 +84,176 @@ export const BibleBooks: IBooks[] = [
         name: 'Song of Songs',
         lastChapter: 8,
     },
+    {
+        name: 'Isaiah',
+        lastChapter: 66,
+    },
+    {
+        name: 'Jeremiah',
+        lastChapter: 52,
+    },
+    {
+        name: 'Lamentations',
+        lastChapter: 5,
+    },
+    {
+        name: 'Ezekiel',
+        lastChapter: 48,
+    },
+    {
+        name: 'Daniel',
+        lastChapter: 12,
+    },
+    {
+        name: 'Hosea',
+        lastChapter: 14,
+    },
+    {
+        name: 'Joel',
+        lastChapter: 3,
+    },
+    {
+        name: 'Amos',
+        lastChapter: 9,
+    },
+    {
+        name: 'Obadiah',
+        lastChapter: 1,
+    },
+    {
+        name: 'Jonah',
+        lastChapter: 4,
+    },
+    {
+        name: 'Micah',
+        lastChapter: 7,
+    },
+    {
+        name: 'Nahum',
+        lastChapter: 3,
+    },
+    {
+        name: 'Habakkuk',
+        lastChapter: 3,
+    },
+    {
+        name: 'Haggai',
+        lastChapter: 12,
+    },
+    {
+        name: 'Zechariah',
+        lastChapter: 14,
+    },
+    {
+        name: 'Malachi',
+        lastChapter: 4,
+    },
+    {
+        name: 'Matthew',
+        lastChapter: 28,
+    },
+    {
+        name: 'Mark',
+        lastChapter: 16,
+    },
+    {
+        name: 'Luke',
+        lastChapter: 24,
+    },
+    {
+        name: 'John',
+        lastChapter: 21,
+    },
+    {
+        name: 'Acts',
+        lastChapter: 28,
+    },
+    {
+        name: 'Romans',
+        lastChapter: 16,
+    },
+    {
+        name: '1 Corinthians',
+        lastChapter: 16,
+    },
+    {
+        name: '2 Corinthians',
+        lastChapter: 13,
+    },
+    {
+        name: 'Galatians',
+        lastChapter: 6,
+    },
+    {
+        name: 'Ephesians',
+        lastChapter: 6,
+    },
+    {
+        name: 'Philippians',
+        lastChapter: 8,
+    },
+    {
+        name: 'Colossians',
+        lastChapter: 4,
+    },
+    {
+        name: '1 Thessalonians',
+        lastChapter: 5,
+    },
+    {
+        name: '2 Thessalonians',
+        lastChapter: 3,
+    },
+    {
+        name: '1 Timothy',
+        lastChapter: 6,
+    },
+    {
+        name: '2 Timothy',
+        lastChapter: 4,
+    },
+    {
+        name: 'Titus',
+        lastChapter: 3,
+    },
+    {
+        name: 'Philemon',
+        lastChapter: 1,
+    },
+    {
+        name: 'Hebrews',
+        lastChapter: 13,
+    },
+    {
+        name: 'James',
+        lastChapter: 5,
+    },
+    {
+        name: '1 Peter',
+        lastChapter: 5,
+    },
+    {
+        name: '2 Peter',
+        lastChapter: 3,
+    },
+    {
+        name: '1 John',
+        lastChapter: 5,
+    },
+    {
+        name: '2 John',
+        lastChapter: 1,
+    },
+    {
+        name: '3 John',
+        lastChapter: 1,
+    },
+    {
+        name: 'Jude',
+        lastChapter: 1,
+    },
+    {
+        name: 'Revelation',
+        lastChapter: 22,
+    },
 ];
