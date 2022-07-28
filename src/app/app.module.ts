4b6d0746f11633c4a6e40f26e6f2f26b8cbb5f65
yongjohnlee80 <yongjohnlee80@outlook.com>
2022-07-28 16:23:06 -0700

    hooks added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { UserModule } from './user/user.module';
-import { NavComponent } from './nav/nav.component';
 import { AngularFireModule } from '@angular/fire/compat';
-import { environment } from 'src/environments/environment';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from 'src/environments/environment';
+
+import { UserModule } from './user/user.module';
+
+// Components
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 import { BibleContentComponent } from './bible-content/bible-content.component';
 
 @NgModule({
